3484b14f3df798247fe6009e88bc3b8f935c3bfa
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-11-21 09:36:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-animal = ""
+let animal = '';
 
 function myAnimal() {
-  return animal
+  animal='dog';
+  return animal;
 }
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  animal='cat';
+  return animal;
 }
-
-function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
-  two = 2
+let two=yourAnimal();
+let n=myAnimal();
+function add2(two, n) {
+return (n + two);
 }
